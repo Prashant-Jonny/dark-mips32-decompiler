3efd231717db939fb21a74136ec5f2b775ddefc0
Filip Allberg <filip@cs.umu.se>
2015-11-01 13:16:32 +0100

    Make project agnostic

--- src/test/java/io/github/gleipner/dark/mips32decomposer/instruction/DecomposedRepresentationTest.java
+++ src/test/java/io/github/gleipner/dark/mips32decomposer/instruction/DecomposedRepresentationTest.java
@@ -1,19 +1,18 @@
 package io.github.gleipner.dark.mips32decomposer.instruction;
 
-import io.github.gleipner.dark.mips32decomposer.instruction.parselet.DecomposedRepresentation;
+import io.github.gleipner.dark.mips32decomposer.util.DecomposedRepresentation;
 import org.junit.Test;
 
-import static io.github.gleipner.dark.mips32decomposer.instruction.TestInstructions.MUL_INSTRUCTION;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class DecomposedRepresentationTest {
     @Test
-    public void shouldDecomposeMulInstructionCorrectly() {
+    public void shouldDecomposeJavadocExampleCorrectly() {
         int[] expected = {0x1c, 8, 1, 9, 0, 2};
         DecomposedRepresentation d = DecomposedRepresentation.fromNumber(
-                MUL_INSTRUCTION, 6, 5, 5, 5, 5, 6);
+                0x71014802, 6, 5, 5, 5, 5, 6);
         int[] actual = d.toIntArray();
 
         assertThat(actual, is(equalTo(expected)));
